c305aaa4d5811599851264aae7d939265f77318a
rishitha6274 <rishithalattupally01@gmail.com>
2025-06-01 22:45:48 +0530

    updated

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 import express from "express";
 import cors from "cors";
+
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+const users = [];
+
 app.listen(8080, () => {
   console.log("Server Started on port 8080");
 });
-
-app.use(express.json());
-app.use(cors());
-const users = [];
 
 app.get("/", (req, res) => {
   return res.json(users);
 });
 
 app.post("/register", (req, res) => {
-  const { name, email, pass } = req.body;
-  users.push({ name, email, pass });
-  return res.json(users);
+  const { name, email, password } = req.body;
+  users.push({ name, email, password });
+  return res.json({ message: "User registered successfully" });
 });
 
 app.post("/login", (req, res) => {
-  const { email, pass } = req.body;
+  const { email, password } = req.body;
   const found = users.find(
-    (value) => value.email === email && value.pass === pass
+    (value) => value.email === email && value.password === password
   );
   if (!found) {
-    res.json({message:"User not found"})
+    return res.status(401).json({ message: "User not found" });
   }
-  res.json({...found,token:"123"});
+  return res.json({ ...found, token: "123" });
 });
