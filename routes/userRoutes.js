43d324f1dd5d38ecc0dc3c264c321cb25abe3fcb
rishitha6274 <rishithalattupally01@gmail.com>
2025-06-06 15:32:46 +0530

    updated

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,17 @@
 import express from 'express'
 import userModel from "../models/userModel.js";
+import bcrypt from 'bcryptjs';
+import jwt from "jsonwebtoken";
+const SECRET_KEY = "helloworld";
 
 const userRouter = express.Router()
 
 userRouter.post("/register", async (req, res) => {
   const { name, email, pass } = req.body;
+  
   try {
-    const result = await userModel.create({ name, email, pass });
+    const hashpassword = await bcrypt.hash(pass, 10);
+    const result = await userModel.create({ name, email, pass:hashpassword });
     res.json({ message: "User registered successfully", user: result });
   } catch (err) {
     console.error("Registration error:", err);
@@ -15,14 +20,19 @@ userRouter.post("/register", async (req, res) => {
 });
 userRouter.post("/login", async (req, res) => {
   const { email, pass } = req.body;
-  const result = await userModel.findOne({ email, pass });
-
+  const result = await userModel.findOne({ email });
   if (!result) return res.json({ message: "Invalid User or Password" });
-
+  const matchPassword = await bcrypt.compare(pass, result.pass);
+  if(!matchPassword){
+    return res.status(400).json({message: "Invalid Password"});
+  }
+  const token = jwt.sign({email: result.email, id: result._id}, SECRET_KEY);
   return res.json({
-    token: "dummy_token_123", 
-    name: result.name,
-    email: result.email
+    user: result, token: token
+    // token: "dummy_token_123", 
+    // name: result.name,
+    // email: result.email,
+    // pass: hashpassword,
   });
 });
 
